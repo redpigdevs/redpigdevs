7b499dea3c8ccc0af1dc0bab20d79df01a041b8f
Administrator <Administrator@10.130.195.125>
2016-08-22 15:28:33 +0900

    step2 code test..

--- Yong/src/step2/PrintAandBTotal.java
+++ Yong/src/step2/PrintAandBTotal.java
@@ -7,7 +7,7 @@ public class PrintAandBTotal {
     @SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+	
 		Scanner scan = new Scanner(System.in);
 		
 		//int a = 7;
--- Yong/src/step2/PrintAdivisionB.java
+++ Yong/src/step2/PrintAdivisionB.java
@@ -7,7 +7,7 @@ public class PrintAdivisionB {
     @SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+	
 		Scanner scan = new Scanner(System.in);
 		
 		//int a = 1;
