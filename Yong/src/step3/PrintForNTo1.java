f09753bdab87798a3a55379cc38c7768d4dcdd67
Administrator <Administrator@10.130.195.125>
2016-08-22 16:17:56 +0900

    N부터 1까지 숫자를 한 줄에 하나씩 출력해봅니다

--- Yong/src/step3/PrintForNTo1.java
+++ Yong/src/step3/PrintForNTo1.java
@@ -7,7 +7,7 @@ public class PrintForNTo1 {
     @SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+	
 		Scanner scan = new Scanner(System.in);
 		
 		//int a = 5;
