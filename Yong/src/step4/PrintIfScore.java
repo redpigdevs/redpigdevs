43fca0ee62042a384cf1b053192627e706d680d7
Administrator <Administrator@10.130.195.125>
2016-08-24 13:28:44 +0900

    시험 점수를 입력받고 성적 출력해보기

--- Yong/src/step4/PrintIfScore.java
+++ Yong/src/step4/PrintIfScore.java
@@ -15,16 +15,16 @@ public class PrintIfScore {
         //90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F
         String scoreRank = "F";
         if(90 <= scoreVal && scoreVal <= 100) {
-        	scoreRank = "A";
+            scoreRank = "A";
         	
         } else if(80 <= scoreVal && scoreVal < 90) {
-        	scoreRank = "B";
+            scoreRank = "B";
         	
         } else if(70 <= scoreVal && scoreVal < 80){
-        	scoreRank = "C";
+            scoreRank = "C";
         	
         } else if(60 <= scoreVal && scoreVal < 70) {
-        	scoreRank = "D";
+            scoreRank = "D";
         }
         System.out.println(scoreRank);
     }
