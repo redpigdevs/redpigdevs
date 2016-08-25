0b59af0d37cdb2f7e45fab944ae153d0dafdb6c2
Administrator <Administrator@10.130.195.125>
2016-08-25 09:33:19 +0900

    세 정수 A, B, C중에 두 번째로 큰 정수 찾아보기

--- Yong/src/step4/PrintIfInteger.java
+++ Yong/src/step4/PrintIfInteger.java
@@ -1,4 +1,4 @@
-package Code_201608;
+package step4;
 
 import java.util.Scanner;
 
@@ -9,30 +9,30 @@ public class PrintIfInteger {
 		
         Scanner scan = new Scanner(System.in);
         
-        int intValA = 20;
-        int intValB = 10;
-        int intValC = 10;
+        //int intValA = 20;
+        //int intValB = 10;
+        //int intValC = 10;
 
-        //int intValA = scan.nextInt();
-        //int intValB = scan.nextInt();
-        //int intValC = scan.nextInt();
+        int intValA = scan.nextInt();
+        int intValB = scan.nextInt();
+        int intValC = scan.nextInt();
         
         //명확히 구분 될 때
         if(((intValA > intValB && intValA < intValC) || (intValA > intValC && intValA < intValB)) && (intValB != intValC)) {
-        	System.out.println(intValA);
+            System.out.println(intValA);
         	
         } else if(((intValB > intValA && intValB < intValC) || (intValB > intValC && intValB < intValA)) && (intValA != intValC)) {
-        	System.out.println(intValB);
+            System.out.println(intValB);
         	
         } else if(((intValC > intValA && intValC < intValB) || (intValC > intValB && intValC < intValA)) && (intValA != intValB) ) {
-        	System.out.println(intValC);
+            System.out.println(intValC);
         	
         //두 수가 같을 때
         } else if((intValA == intValB) || (intValA == intValC)) {
-        	System.out.println(intValA);
+            System.out.println(intValA);
         	
         } else if((intValB == intValC)) {
-        	System.out.println(intValB);
+            System.out.println(intValB);
         	
         //세 수가 같을 때
         } else if((intValA == intValB) && (intValA == intValC) && (intValB == intValC)) {
