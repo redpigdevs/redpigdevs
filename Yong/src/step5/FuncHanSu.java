d8bdea5b303b76598143e3bec72e76b05a3fd087
Administrator <Administrator@10.130.195.125>
2016-09-06 13:20:58 +0900

    자연수 n에 대해 d(n)의 값을 구하는 함수를 정의해 문제를 해결해봅니다

---
+++ Yong/src/step5/FuncHanSu.java
@@ -0,0 +1,34 @@
+package step5;
+
+import java.util.Scanner;
+
+public class FuncHanSu {
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+		
+        Scanner scan = new Scanner(System.in);
+        
+        //int valA = 110;
+        int valA = scan.nextInt();
+        int valK = 0;
+        int valHan = 0;
+        int varArr[] = new int[3];
+		
+        for(int i = 1; i <= valA; i++) {
+            if(0 < i && i < 100) valHan = i;
+            else if(i == 1000) break;
+            else {
+                valK = 0;
+                int valT = i;
+                while(valT > 0) {
+                    varArr[valK] = valT % 10;
+                    valT /= 10; valK++;
+                }
+                if(varArr[0] - varArr[1] == varArr[1] - varArr[2]) valHan++;
+            }
+        }
+        System.out.println(valHan);
+    }
+
+}//
