6d2c6b20a9639405a3eeb68f3f5ae419b01b0d1d
Administrator <Administrator@10.130.195.125>
2016-09-06 14:13:33 +0900

    각 자리수가 등차수열을 이루는 지 판별하는 함수를 구현해 문제를 해결해봅니다

--- Yong/src/step5/FuncHanSu.java
+++ Yong/src/step5/FuncHanSu.java
@@ -16,16 +16,22 @@ public class FuncHanSu {
         int varArr[] = new int[3];
 		
         for(int i = 1; i <= valA; i++) {
-            if(0 < i && i < 100) valHan = i;
-            else if(i == 1000) break;
-            else {
+            if(0 < i && i < 100) {
+                valHan = i;
+            
+            } else if(i == 1000) {
+                break;
+            
+            } else {
                 valK = 0;
                 int valT = i;
                 while(valT > 0) {
                     varArr[valK] = valT % 10;
                     valT /= 10; valK++;
                 }
-                if(varArr[0] - varArr[1] == varArr[1] - varArr[2]) valHan++;
+                if(varArr[0] - varArr[1] == varArr[1] - varArr[2]) {
+                    valHan++;
+                }
             }
         }
         System.out.println(valHan);
