647872826f80681e3275bcd6fb9a011d54f95098
Administrator <Administrator@10.130.195.125>
2016-09-06 14:03:51 +0900

    각 자리수가 등차수열을 이루는 지 판별하는 함수를 구현해 문제를 해결해봅니다

--- Yong/src/step5/FuncHanSu.java
+++ Yong/src/step5/FuncHanSu.java
@@ -6,7 +6,7 @@ public class FuncHanSu {
 
     @SuppressWarnings("resource")
     public static void main(String[] args) {
-	
+
         Scanner scan = new Scanner(System.in);
         
         //int valA = 110;
